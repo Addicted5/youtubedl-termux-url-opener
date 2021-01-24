c8e64262531fe0652c626100e0116863cd9fb2f5
Addicted5 <ashutoshprusty29@gmail.com>
2021-01-24 22:42:14 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ rm -f ~/bin/termux-url-opener
 mkdir ~/bin
 #Installing dependencies
 pkg update -y && pkg install wget -y && pkg install python -y && pkg install ffmpeg -y
-yes | pip install youtube-dl && yes | pip install spotdl && yes | pip install youtube-dlc && yes | pip install you-get
+yes | pip install -U youtube-dl && yes | pip install -U spotdl && yes | pip install -U youtube-dlc && yes | pip install -U you-get
 
 #Installing the script
 wget -P ~/bin/ --no-check-certificate https://raw.githubusercontent.com/Addicted5/youtubedl-termux-url-opener/main/termux-url-opener
